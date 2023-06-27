bc23b36453621976be83626c392e04916a86d48b
catboshi <catboshi@gmail.com>
2023-06-27 17:09:33 +0800

    dubbo-wecom公共dubbo服务接口

---
+++ dubbo-wecom/src/main/java/tech/wedev/dubbo/wecom/service/dubboWecomRpcService.java
@@ -0,0 +1,7 @@
+package tech.wedev.dubbo.wecom.service;
+
+import java.util.Map;
+
+public interface dubboWecomRpcService {
+    Map<String, Object> getApiExternalContactGet(Map<String, Object> body);
+}
