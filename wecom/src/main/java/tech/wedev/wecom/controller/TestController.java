3d4b114e3c410e19a52bb6cffa805b941120c880
catboshi <catboshi@gmail.com>
2023-08-19 18:04:58 +0800

    add: 远程监控JVM Dockerfile配置

--- wecom/src/main/java/tech/wedev/wecom/controller/TestController.java
+++ wecom/src/main/java/tech/wedev/wecom/controller/TestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.wedev.wecom.annos.StopWatch;
 import tech.wedev.wecom.third.WecomRequestService;
 import tech.wedev.wecom.utils.RedisUtils;
 
@@ -30,8 +31,28 @@ public class TestController {
         return result;
     }
 
+    @StopWatch
     @RequestMapping("/externalcontact/get/{external_userid}")
     public Map<String, Object> getExternalContact(@PathVariable String external_userid) {
         return wecomRequestService.externalContactGet("wwbda3d4206748805c",external_userid);
+    }
+
+    @StopWatch
+    @RequestMapping("/test/aop/stopwatch")
+    public String stopWatch() {
+        System.out.println("testAopStopWatch");
+        return "testAopStopWatch";
+    }
+
+    @RequestMapping("/test/jrebel")
+    public String testJrebel() {
+        System.out.println("testJrebel");
+        return "testJrebel";
+    }
+
+    @RequestMapping("/test/jrebel/remote")
+    public String testJrebelRemote() {
+        System.out.println("testJrebelRemote");
+        return "testJrebelRemote";
     }
 }
